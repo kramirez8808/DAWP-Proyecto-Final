a6a8d71d5738bde21e32e0743977e2b01aa7a303
JeffryCH <jcanales20717@ufide.ac.cr>
2024-03-05 12:26:13 -0600

    Creación del proyecto tienda, modificación de properties y index.html

--- proyecto/src/main/java/dawp/proyecto/controller/IndexController.java
+++ proyecto/src/main/java/dawp/proyecto/controller/IndexController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    
+
     @RequestMapping("/")
     public String index(Model model) {
-        
+
         return "index";
     }
 }
